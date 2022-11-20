82a8768cbf2c27d6348fa1c97566ed19f38e2c7e
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-11-20 21:58:13 +0700

    add hamburger button

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-max-depth */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
 import List from "@components/navbar/items/list.component";
 import Logo from "@components/navbar/items/logo.component";
@@ -8,6 +10,12 @@ import Icons from "./items/icons.module";
 const Navbar: React.FC = () => (
   <div className={styles.wrapper}>
     <div className={styles.container}>
+      <label
+        className={styles.menu}
+        htmlFor="menu__toggle">
+        <span />
+      </label>
+
       <Logo />
 
       <List />
