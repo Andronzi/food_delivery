40ca5eb25fd94447006a8347fd4cf1bc2c3ec8be
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-11-21 13:13:10 +0700

    Task #15335 change styles for hamburger add default reset to index.css

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,20 +1,15 @@
-/* eslint-disable react/jsx-max-depth */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
 import List from "@components/navbar/items/list.component";
 import Logo from "@components/navbar/items/logo.component";
 import Location from "./items/location.component";
 import styles from "./navbar.module.scss";
 import Icons from "./items/icons.module";
+import Hamburger from "./items/hamburger.component";
 
 const Navbar: React.FC = () => (
   <div className={styles.wrapper}>
     <div className={styles.container}>
-      <label
-        className={styles.menu}
-        htmlFor="menu__toggle">
-        <span />
-      </label>
+      <Hamburger />
 
       <Logo />
 
