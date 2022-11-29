000ee1cc73eec174cacd53a4aab7af5811264d1e
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-11-29 17:46:56 +0700

    Task #15445 create routeConfig with one route

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import reactDom from "react-dom";
-import App from "./components/app/app.component";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "@src/store";
+import { store } from "@redux/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./components/pages/app/app.component";
 import "./index.css";
 
-reactDom.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+
+export const routerConfig = [
+  {
+    path: "/:page",
+    element: <App />,
+  },
+];
+
+const router = createBrowserRouter(routerConfig);
+
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
