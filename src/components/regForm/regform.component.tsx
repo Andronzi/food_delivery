7e46b5c840db3a718ed3f1fd78bcfd56f7beff8d
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-12-06 15:42:38 +0700

    TASK #15453 create basic form make select and errors create styles for errors delete strange eslint conditions

--- src/components/regForm/regform.component.tsx
+++ src/components/regForm/regform.component.tsx
@@ -1,34 +1,97 @@
-import Input from "@components/ui/input/input.component";
+// import Input from "@components/ui/input/input.component";
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 import styles from "./form.module.scss";
 
 interface IFormInput {
-  name: string;
+  fullName: string;
   gender: string;
-  phone: number;
+  email: string;
+  password: string;
 }
 
 const RegForm = () => {
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>({
+    criteriaMode: "all",
+  });
   const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
 
   return (
     <form
       className={styles.form}
       onSubmit={handleSubmit(onSubmit)}>
-      <Input
+      <input
         placeholder="ФИО"
-        register={register}
-        required
-        value="name"
+        {...register("fullName", {
+          required: true,
+        })}
       />
 
-      <Input
-        placeholder="Пол"
-        register={register}
-        required
-        value="gender"
+      {errors.fullName ? (
+        <p className={styles.error}>ФИО является обязательным</p>
+      ) : null}
+
+      <select
+        className={styles.select}
+        {...register("gender")}>
+        <option value="Female">Жен</option>
+
+        <option value="Male">Муж</option>
+      </select>
+
+      <input
+        placeholder="Email"
+        {...register("email", {
+          required: "Данное поле является обязательным",
+          pattern: {
+            value: /[a-z0-9]+@[a-z]+.[a-z]{2,3}/i,
+            message: "Email неверный",
+          },
+        })}
+      />
+
+      <ErrorMessage
+        errors={errors}
+        name="email"
+        render={({ messages }) =>
+          messages
+            ? Object.entries(messages).map(([type, message]) => (
+                <p
+                  key={type}
+                  className={styles.error}>
+                  {message}
+                </p>
+              ))
+            : null
+        }
+      />
+
+      <input
+        placeholder="Пароль"
+        {...register("password", {
+          required: "Данное поле является обязательным",
+        })}
+      />
+
+      <ErrorMessage
+        errors={errors}
+        name="password"
+        render={({ messages }) =>
+          messages
+            ? Object.entries(messages).map(([type, message]) => (
+                <p
+                  key={type}
+                  className={styles.error}>
+                  {message}
+                </p>
+              ))
+            : null
+        }
       />
 
       <input
--- src/components/ui/input/input.component.tsx
+++ src/components/ui/input/input.component.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { UseFormRegister } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 import styles from "./input.module.scss";
 
 interface InputProps {
   label?: string;
   placeholder?: string;
+  pattern?: { value: RegExp; message: string };
+  errors?: any;
   register: UseFormRegister<any>;
   value: string;
   required: boolean;
@@ -13,9 +16,11 @@ interface InputProps {
 const Input = ({
   label,
   placeholder,
+  pattern,
   register,
   value,
   required,
+  errors,
 }: InputProps) => (
   <>
     <label>{label}</label>
@@ -23,7 +28,19 @@ const Input = ({
     <input
       className={styles.input}
       placeholder={placeholder}
-      {...register(value, { required })}
+      {...register(value, { required, pattern })}
+    />
+
+    <ErrorMessage
+      errors={errors}
+      name={value}
+      render={({ messages }) =>
+        messages
+          ? Object.entries(messages).map(([type, message]) => (
+              <p key={type}>{message}</p>
+            ))
+          : null
+      }
     />
   </>
 );
