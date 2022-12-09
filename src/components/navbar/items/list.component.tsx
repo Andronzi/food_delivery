d000b75a7cbc458297740d1a91e1399614e49e81
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-12-10 01:46:54 +0700

    Task #15452 and Task #15335 add profile window and add nav list in nav list and in profile use Link from react-router-dom also in profile window we draw it depends on user data existing

--- src/components/navbar/items/list.component.tsx
+++ src/components/navbar/items/list.component.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "@components/navbar/navbar.module.scss";
+import { Link } from "react-router-dom";
 
 const List = (): JSX.Element => (
   <ul className={styles.ul}>
     <li>
-      <a href="/menu">Меню</a>
+      <Link to="/">Меню</Link>
     </li>
 
     <li>
-      <a href="/orders">Заказы</a>
+      <Link to="/orders">Заказы</Link>
     </li>
   </ul>
 );
