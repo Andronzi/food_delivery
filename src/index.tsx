4cb0cd003fd3b4987852940fa5bb6d939623cc4f
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-12-09 20:34:14 +0700

    task #15435 create routes in app component and provide them to browserrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "@redux/store";
+<<<<<<< Updated upstream
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/pages/app/app.component";
+=======
+import App from "@components/pages/app/app.component";
+import { BrowserRouter } from "react-router-dom";
+>>>>>>> Stashed changes
 import "./index.css";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
+<<<<<<< Updated upstream
 export const routerConfig = [
   {
     path: "/:page",
@@ -21,5 +27,12 @@ const router = createBrowserRouter(routerConfig);
 root.render(
   <Provider store={store}>
     <RouterProvider router={router} />
+=======
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+>>>>>>> Stashed changes
   </Provider>,
 );
