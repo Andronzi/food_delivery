55b0c02c0a59f9ecbd15af3a77a08a9d98e333a4
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-12-09 20:32:13 +0700

    Task #16013 create toggle block in icons component

--- src/components/navbar/items/icons.module.tsx
+++ src/components/navbar/items/icons.module.tsx
@@ -2,21 +2,60 @@ import React from "react";
 import user from "@icons/user.svg";
 import cart from "@icons/cart.svg";
 import styles from "@components/navbar/navbar.module.scss";
+import { Link } from "react-router-dom";
 
-const Icons = (): JSX.Element => (
-  <div className={styles.icons}>
-    <img
-      alt={user}
-      className={styles.user}
-      src={user}
-    />
+interface Props {
+  name: string | boolean;
+}
 
-    <img
-      alt={cart}
-      className={styles.cart}
-      src={cart}
-    />
-  </div>
-);
+const Icons = ({ name }: Props): JSX.Element => {
+  const [visibility, setVisibility] = React.useState(false);
+  const handleProfileImageClick = () => {
+    setVisibility(prevState => !prevState);
+  };
+
+  return (
+    <div className={styles.icons}>
+      <img
+        onClick={handleProfileImageClick}
+        alt={user}
+        className={styles.user}
+        src={user}
+      />
+      <div
+        className={`${styles.userPanel} ${
+          visibility === true
+            ? styles.userPanelVisible
+            : styles.userPanelUnVisible
+        }`}>
+        <p className={styles.title}>{name ? name : "Гость"}</p>
+        {name === false ? (
+          <>
+            <Link to="registration">
+              <p className={styles.text}>Зарегистрироваться</p>
+            </Link>
+            <Link to="auth">
+              <p className={styles.text}>Авторизоваться</p>
+            </Link>
+          </>
+        ) : (
+          <>
+            <p className={styles.text}>Мои данные</p>
+            <Link to="orders">
+              <p className={styles.text}>Мои заказы</p>
+            </Link>
+            <p className={styles.text}>Выйти</p>
+          </>
+        )}
+      </div>
+
+      <img
+        alt={cart}
+        className={styles.cart}
+        src={cart}
+      />
+    </div>
+  );
+};
 
 export default Icons;
