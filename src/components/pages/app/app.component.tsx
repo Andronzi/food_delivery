507939c0523fe585102ffc512152833ef7ad960d
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-12-09 20:47:06 +0700

    merge app file after conflicts

--- src/components/pages/app/app.component.tsx
+++ src/components/pages/app/app.component.tsx
@@ -2,21 +2,15 @@ import React from "react";
 import "@components/ui/button/button.module.scss";
 import Navbar from "@components/navbar/navbar.component";
 import Food from "@components/food/food.component";
-<<<<<<< HEAD:src/components/pages/app/app.component.tsx
-=======
 import { Route, Routes } from "react-router-dom";
 import Authorisation from "../registration/auth.component";
 import Registration from "../registration/registration.component";
 import NotFound from "../404/404.component";
->>>>>>> feature/redux:src/components/app/app.component.tsx
 
 const App = () => (
   <div className="app">
     <Navbar />
 
-<<<<<<< HEAD:src/components/pages/app/app.component.tsx
-    <Food />
-=======
     <Routes>
       <Route
         element={<Food />}
@@ -27,16 +21,17 @@ const App = () => (
         element={<Authorisation />}
         path="auth"
       />
+
       <Route
         element={<Registration />}
         path="registration"
       />
+
       <Route
-        path="/*"
+        path="*"
         element={<NotFound />}
       />
     </Routes>
->>>>>>> feature/redux:src/components/app/app.component.tsx
   </div>
 );
 
