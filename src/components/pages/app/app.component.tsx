f680d7a48d1fe51b1031ef5d8127c38cd4b5ed45
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-12-10 15:19:27 +0700

    task #15452 add getprofile to app

--- src/components/pages/app/app.component.tsx
+++ src/components/pages/app/app.component.tsx
@@ -3,36 +3,45 @@ import "@components/ui/button/button.module.scss";
 import Navbar from "@components/navbar/navbar.component";
 import Food from "@components/food/food.component";
 import { Route, Routes } from "react-router-dom";
+import { useAppDispatch } from "@redux/hooks/hooks";
+import { getProfile } from "@redux/slices/profileSlice";
 import Authorisation from "../registration/auth.component";
 import Registration from "../registration/registration.component";
 import NotFound from "../404/404.component";
 
-const App = () => (
-  <div className="app">
-    <Navbar />
+const App = () => {
+  const dispatch = useAppDispatch();
+  React.useEffect(() => {
+    dispatch(getProfile(localStorage.getItem("token")!));
+  }, [dispatch]);
 
-    <Routes>
-      <Route
-        element={<Food />}
-        path="/"
-      />
+  return (
+    <div className="app">
+      <Navbar />
 
-      <Route
-        element={<Authorisation />}
-        path="auth"
-      />
+      <Routes>
+        <Route
+          element={<Food />}
+          path="/"
+        />
 
-      <Route
-        element={<Registration />}
-        path="registration"
-      />
+        <Route
+          element={<Authorisation />}
+          path="auth"
+        />
 
-      <Route
-        element={<NotFound />}
-        path="*"
-      />
-    </Routes>
-  </div>
-);
+        <Route
+          element={<Registration />}
+          path="registration"
+        />
+
+        <Route
+          element={<NotFound />}
+          path="*"
+        />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
