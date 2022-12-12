fa0f658be787d6d18daf1bd6bf8a9538ceca7307
Radionov Andrew <andrey.radioniv.2003@gmail.com>
2022-12-12 16:23:54 +0700

    resolve conflict after merge

--- src/index.tsx
+++ src/index.tsx
@@ -2,37 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "@redux/store";
-<<<<<<< HEAD
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./components/pages/app/app.component";
-=======
-import App from "./components/pages/app/app.component";
 import { BrowserRouter } from "react-router-dom";
->>>>>>> feature/redux_cart
+import App from "./components/pages/app/app.component";
 import "./index.css";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
-<<<<<<< HEAD
-export const routerConfig = [
-  {
-    path: "/:page",
-    element: <App />,
-  },
-];
-
-const router = createBrowserRouter(routerConfig);
-
-root.render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-=======
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
->>>>>>> feature/redux_cart
   </Provider>,
 );
